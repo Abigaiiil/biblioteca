b2a703b4260f69942cd33bf03b7affb1d38021ff
Carolina Abigail Angeles Santiago <L20260179@matamoros.tecnm.mx>
2026-03-19 15:51:54 -0500

    22: Devolución de Libros | Actualización de Estados y Fecha de Entrega

--- resources/views/prestamos/index.blade.php
+++ resources/views/prestamos/index.blade.php
@@ -20,7 +20,9 @@
                 <th class="px-4 py-2 border-b">ID</th>
                 <th class="px-4 py-2 border-b">Libro</th>
                 <th class="px-4 py-2 border-b">Usuario</th>
-                <th class="px-4 py-2 border-b">Fecha de Préstamo</th>
+                <th class="px-4 py-2 border-b">Fecha</th>
+                <th class="px-4 py-2 border-b">Estatus</th>
+                <th class="px-4 py-2 border-b">Fecha de Entrega</th>
                 <th class="px-4 py-2 border-b">Acciones</th>
                 </tr>
                 </thead>
@@ -33,7 +35,19 @@
                             <td class="px-6 py-4 whitespace-nowrap">{{ $prestamo->usuario->name }}</td>
                             <td class="px-6 py-4 whitespace-nowrap">{{ $prestamo->created_at->format('Y-m-d') }}</td>
                             <td class="px-6 py-4 whitespace-nowrap">
-                            
+                                @if($prestamo->estado == 'pendiente')
+                                    <span class="bg-red-100 text-red-700 px-2 py-1 rounded-full text-xs">Pendiente</span>
+                                @else
+                                    <span class="bg-green-100 text-green-700 px-2 py-1 rounded-full text-xs">Entregado</span>
+                                @endif
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                {{ $prestamo->fecha_entrega ? $prestamo->fecha_entrega : '' }}
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                @if($prestamo->estado == 'pendiente')
+                                <a href = "{{ route('prestamos.entregar', $prestamo->id) }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4 inline-block">Entregar</a>
+                                @endif
                             </td>
                         </tr>
                     @endforeach
